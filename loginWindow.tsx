e2ad328fb1a9d025957c270033840a86665466e8
frankngugi <101904854+frankngugi@users.noreply.github.com>
2023-11-17 19:13:30 +0300

    Add files via upload

---
+++ loginWindow.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { useState } from "react";
+
+export default function LoginWindow(
+  { setUpProfile }: {
+    setUpProfile:  (form: FormData) => Promise<void>;
+  }
+) {
+
+  /* 
+    #1: Add a state variable to store the current error message
+  */
+ const[error, setError ] = useState<string>("");
+
+
+
+  /*
+    #2: This function is called to set up the profile of the new user. It is retreived
+    when a user submits the login form. It takes a FormData object as an argument,
+    which contains the username and name of the new user.
+    @param form - FormData object containing the username and name of the new user.
+  */
+  const onSubmit = async (form: FormData) => {
+    /* 
+      #3: Set the error state to an empty string.
+    */
+   setError("");
+    /* 
+      #4: Set up a try catch block to call the setUpProfile() function and set the error state
+      if an error is thrown.
+      HINT: 
+        - Use the setUpProfile() function to set up the user's profile and log them in
+        - In the catch block, set the error state to the error message (error.message)
+    */
+   try{
+    await setUpProfile(form); //call the setupProfile() function.
+   }catch(error: any){
+    setError(error.message); //set the error state to the error message.
+   }
+  };
+
+  return (
+    <form action={onSubmit} >
+      <div className='flex flex-col space-y-3'>
+        <p className='text-xs font-bold uppercase text-neutral-100'>
+          Create Account
+        </p>
+        <input
+          id='username'
+          type='text'
+          name='username'
+          className='rounded border border-neutral-200 bg-neutral-300 px-3 py-2 text-sm'
+          placeholder='Username'
+          required
+        />
+        <input
+          id='name'
+          type='text'
+          name='name'
+          className='rounded border border-neutral-200 bg-neutral-300 px-3 py-2 text-sm'
+          placeholder='Name'
+          required
+        />
+        <div className="w-full">
+          <button
+            type='submit'
+            className='w-full rounded bg-blue-500 py-2.5 text-sm font-medium hover:bg-blue-400 flex flex-row justify-center items-center space-x-2'
+          >
+            Create Account
+          </button>
+          <p className="text-red-500">
+            {/* 
+              #2: Display the error message if it is not an empty string using the error state variable.
+            */}
+            {error}
+          </p>
+        </div>
+      </div>
+    </form>
+  )
+}
